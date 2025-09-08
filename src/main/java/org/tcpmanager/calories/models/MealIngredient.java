c269671d3ec7b871a697650da97f79fca00b4a43
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-09-09 01:49:58 +0200

    Update MealIngredient class and upgrade Spring Boot version to 4.0.0-M2

--- src/main/java/org/tcpmanager/calories/models/MealIngredient.java
+++ src/main/java/org/tcpmanager/calories/models/MealIngredient.java
@@ -1,6 +1,5 @@
 package org.tcpmanager.calories.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
