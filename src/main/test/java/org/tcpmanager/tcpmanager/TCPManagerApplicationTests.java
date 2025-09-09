d36afb6d6e3f579742c6994b5c8d749a81f70301
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-09-09 17:04:54 +0200

    Add unit tests for TCPManager application context verification

---
+++ src/main/test/java/org/tcpmanager/tcpmanager/TCPManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tcpmanager.tcpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+
+@SpringBootTest
+class TCPManagerApplicationTests {
+
+  static ApplicationModules applicationModules = ApplicationModules.of(TcpManagerApplication.class);
+
+  @Test
+  void contextLoads() {
+    applicationModules.verify();
+  }
+}
