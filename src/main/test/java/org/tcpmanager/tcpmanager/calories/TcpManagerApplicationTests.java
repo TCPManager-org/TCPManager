29bdae35daa4647ce1a8e60476c6e1d76ae90ad3
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-10-09 16:53:53 +0200

    Add H2 database dependency and configure test properties for contextLoads test

--- src/main/test/java/org/tcpmanager/tcpmanager/calories/TcpManagerApplicationTests.java
+++ src/main/test/java/org/tcpmanager/tcpmanager/calories/TcpManagerApplicationTests.java
@@ -3,9 +3,13 @@ package org.tcpmanager.tcpmanager.calories;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.TestPropertySource;
 import org.tcpmanager.tcpmanager.TcpManagerApplication;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver", "spring.flyway.enabled=false",
+    "spring.jpa.hibernate.ddl-auto=none"})
 class TcpManagerApplicationTests {
 
   static ApplicationModules applicationModules = ApplicationModules.of(TcpManagerApplication.class);
