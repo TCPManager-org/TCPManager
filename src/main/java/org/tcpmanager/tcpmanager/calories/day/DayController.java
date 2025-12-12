697addb4fed0d98c8a17b3b077d1a47bfb4801fc
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-12-12 23:30:50 +0100

    Refactor DayController and DayService to use Principal for user identification; update methods to filter and manage days and meals based on the authenticated user

--- src/main/java/org/tcpmanager/tcpmanager/calories/day/DayController.java
+++ src/main/java/org/tcpmanager/tcpmanager/calories/day/DayController.java
@@ -1,6 +1,7 @@
 package org.tcpmanager.tcpmanager.calories.day;
 
 import jakarta.validation.Valid;
+import java.security.Principal;
 import java.sql.Date;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.tcpmanager.tcpmanager.calories.day.dto.DayMealPatch;
@@ -28,34 +28,35 @@ public class DayController {
 
   @GetMapping(produces = "application/json")
   @ResponseStatus(HttpStatus.OK)
-  public List<DayResponse> getAllDays() {
-    return dayService.getAllDays();
+  public List<DayResponse> getAllDays(Principal principal) {
+    return dayService.getAllDays(principal.getName());
   }
 
   @PostMapping(produces = "application/json")
   @ResponseStatus(HttpStatus.CREATED)
-  public DayResponse addMealToDay(@RequestBody @Valid DayMealRequest dayMealRequest) {
-    return dayService.addMealToDay(dayMealRequest);
+  public DayResponse addMealToDay(@RequestBody @Valid DayMealRequest dayMealRequest,
+      Principal principal) {
+    return dayService.addMealToDay(dayMealRequest, principal.getName());
   }
 
   @DeleteMapping("/{date}/{dayMealId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteMealFromDay(@PathVariable Date date, @PathVariable Long dayMealId,
-      @RequestParam String username) {
-    dayService.deleteMealFromDay(date, dayMealId, username);
+      Principal principal) {
+    dayService.deleteMealFromDay(date, dayMealId, principal.getName());
   }
 
   @PatchMapping("/{date}/{dayMealId}")
   @ResponseStatus(HttpStatus.OK)
   public DayResponse updateMealFromDay(@PathVariable Date date, @PathVariable Long dayMealId,
-      @RequestParam String username, @RequestBody @Valid DayMealPatch dayMealPatch) {
-    return dayService.updateMealFromDay(date, dayMealId, username, dayMealPatch);
+      Principal principal, @RequestBody @Valid DayMealPatch dayMealPatch) {
+    return dayService.updateMealFromDay(date, dayMealId, principal.getName(), dayMealPatch);
   }
 
   @DeleteMapping("/{date}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteDayByDate(@RequestParam String username, @PathVariable Date date) {
-    dayService.deleteByDate(username, date);
+  public void deleteDayByDate(Principal principal, @PathVariable Date date) {
+    dayService.deleteByDate(principal.getName(), date);
   }
 
 }
